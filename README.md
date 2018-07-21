eb947f95fda7e7605fb135e1c0f540fd48584d4e
Mateusz <gorzanski.mateusz@gmail.com>
2018-07-21 19:42:29 +0200

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# Personal Website
-This is my personal website repository. You can visit it [here](https://mgorzanski.github.io).
--- gatsby-config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  siteMetadata: {
-    title: 'Mateusz Górzański',
-  },
-  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-react-next'],
-}
