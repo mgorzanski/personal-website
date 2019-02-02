2259fc4acabc03ba951464bf55512e84717ca920
Mateusz <gorzanski.mateusz@gmail.com>
2019-02-02 19:37:13 +0100

    add code

---
+++ portfolio.php
@@ -0,0 +1,82 @@
+	<?php $loadScripts = true; ?>
+	<?php include 'template/header.php'; ?>
+
+	<main class="site-content empty-space">
+		<div class="row center-lg center-md center-sm center-xs">
+			<div class="col-lg-7 col-md-11 col-sm-11 col-xs-11">
+				<div class="box">
+					<section class="portfolio">
+						<h2 class="section-title">Portfolio</h2>
+						<div class="row portfolio-items">
+							<div class="col-lg-3 col-md-6 col-sm-6 col-xs-6 portfolio-item">
+								<div class="box">
+									<a class="portfolio-item-link" href="/matgor4/portfolio/item/design-for-wordpress-com">
+										<img class="portfolio-item-thumbnail image" src="images/thumbnail.jpg" alt="Design for wordpress.com">
+										<h4 class="portfolio-item-title">Design for wordpress.com</h4>
+									</a>
+								</div>
+							</div>
+							<div class="col-lg-3 col-md-6 col-sm-6 col-xs-6 portfolio-item">
+								<div class="box">
+									<a class="portfolio-item-link" href="/matgor4/portfolio/item/design-for-wordpress-com">
+										<img class="portfolio-item-thumbnail image" src="images/thumbnail.jpg" alt="Design for wordpress.com">
+										<h4 class="portfolio-item-title">Design for wordpress.com</h4>
+									</a>
+								</div>
+							</div>
+							<div class="col-lg-3 col-md-6 col-sm-6 col-xs-6 portfolio-item">
+								<div class="box">
+									<a class="portfolio-item-link" href="/matgor4/portfolio/item/design-for-wordpress-com">
+										<img class="portfolio-item-thumbnail image" src="images/thumbnail.jpg" alt="Design for wordpress.com">
+										<h4 class="portfolio-item-title">Design for wordpress.com</h4>
+									</a>
+								</div>
+							</div>
+							<div class="col-lg-3 col-md-6 col-sm-6 col-xs-6 portfolio-item">
+								<div class="box">
+									<a class="portfolio-item-link" href="/matgor4/portfolio/item/design-for-wordpress-com">
+										<img class="portfolio-item-thumbnail image" src="images/thumbnail.jpg" alt="Design for wordpress.com">
+										<h4 class="portfolio-item-title">Design for wordpress.com</h4>
+									</a>
+								</div>
+							</div>
+							<div class="col-lg-3 col-md-6 col-sm-6 col-xs-6 portfolio-item">
+								<div class="box">
+									<a class="portfolio-item-link" href="/matgor4/portfolio/item/design-for-wordpress-com">
+										<img class="portfolio-item-thumbnail image" src="images/thumbnail.jpg" alt="Design for wordpress.com">
+										<h4 class="portfolio-item-title">Design for wordpress.com</h4>
+									</a>
+								</div>
+							</div>
+							<div class="col-lg-3 col-md-6 col-sm-6 col-xs-6 portfolio-item">
+								<div class="box">
+									<a class="portfolio-item-link" href="/matgor4/portfolio/item/design-for-wordpress-com">
+										<img class="portfolio-item-thumbnail image" src="images/thumbnail.jpg" alt="Design for wordpress.com">
+										<h4 class="portfolio-item-title">Design for wordpress.com</h4>
+									</a>
+								</div>
+							</div>
+							<div class="col-lg-3 col-md-6 col-sm-6 col-xs-6 portfolio-item">
+								<div class="box">
+									<a class="portfolio-item-link" href="/matgor4/portfolio/item/design-for-wordpress-com">
+										<img class="portfolio-item-thumbnail image" src="images/thumbnail.jpg" alt="Design for wordpress.com">
+										<h4 class="portfolio-item-title">Design for wordpress.com</h4>
+									</a>
+								</div>
+							</div>
+							<div class="col-lg-3 col-md-6 col-sm-6 col-xs-6 portfolio-item">
+								<div class="box">
+									<a class="portfolio-item-link" href="/matgor4/portfolio/item/design-for-wordpress-com">
+										<img class="portfolio-item-thumbnail image" src="images/thumbnail.jpg" alt="Design for wordpress.com">
+										<h4 class="portfolio-item-title">Design for wordpress.com</h4>
+									</a>
+								</div>
+							</div>
+						</div>
+					</section>
+				</div>
+			</div>
+		</div>
+	</main>
+
+	<?php include 'template/footer.php'; ?>
